860ab8be32aae98d907c5c0283f47165292784bf
Daniel Webber <dwebber1@users.noreply.github.com>
2016-03-11 14:25:38 -0800

    adding led and button stuff

--- Code/servotest/servotest.ino
+++ Code/servotest/servotest.ino
@@ -1,11 +1,12 @@
 #include <Servo.h> 
-
+int green = 2;
+int red = 3;
 int left = 11;
 int claw =9;
 int right = 6;
 int flex = A0;
 
-//int button = ;
+int button = 12;
 
 int flexstore=0;
 int garbagecount = 0;
@@ -21,13 +22,26 @@ void setup()
   leftservo.attach(left);
   rightservo.attach(right);
   clawservo.attach(claw);
-//  pinMode(button,INPUT);
+  pinMode(button,INPUT_PULLUP);
   pinMode(flex,INPUT);
+  pinMode(red,OUTPUT);
+  pinMode(green,OUTPUT);
   Serial.begin(9600);
   clawservo.write(90);
 
 leftservo.write(92);
 rightservo.write(92);
+
+digitalWrite(red,1);
+digitalWrite(green,1);
+delay(1000);
+digitalWrite(red,0);
+delay(1000);
+digitalWrite(red,1);
+digitalWrite(green,1);
+
+
+
   
   
     // set servo to mid-point
