38036e69be11d4a591ac6e4e2dd6fc2df7e6ff0a
Daniel Webber <dwebber1@users.noreply.github.com>
2016-03-11 17:19:08 -0800

    Finial Code

--- Code/servotest/servotest.ino
+++ Code/servotest/servotest.ino
@@ -5,13 +5,10 @@ int left = 11;
 int claw =9;
 int right = 6;
 int flex = A0;
-
 int button = 12;
-
 int flexstore=0;
 int garbagecount = 0;
-int shouldmove = 1;
-
+boolean hascan = false;
 Servo leftservo;
 Servo rightservo;
 Servo clawservo;
@@ -40,61 +37,74 @@ digitalWrite(green,0);
 delay(1000);
 digitalWrite(red,1);
 digitalWrite(green,1);
-
-
+while (digitalRead(button))
+{}
+delay (1500);
+leftservo.write(0);
+rightservo.write(180);
 
   
   
     // set servo to mid-point
 } 
   
-int isflexed (int input)
+boolean isflexed ()
 {
-    if (input > 170)
-      return 0;
-    return 1;
+    
+    return (analogRead(flex) <= 170);
+    
   
  }
 void loop() 
 {
  //if (digitalRead(button,HIGH){
    // shouldmove = 1;    }
-  if (shouldmove>0)
-    {
-      leftservo.write(0);
-      rightservo.write(180);
-    }
+ 
   
-  if (isflexed(analogRead(flex))>0)
-   {
-      while (garbagecount < 200){
-        Serial.println(garbagecount);
-        if (isflexed (analogRead(flex))==0)
-          garbagecount++;
-        else
-          garbagecount = 0;
-      }
+  if (!hascan && isflexed())
+  {
+    while (garbagecount < 200)
+    {
+      Serial.println(garbagecount);
+      if (!isflexed ())
+        garbagecount++;
+      else
+        garbagecount = 0;
+    }
+      closeclaw();
+  }
+  if(hascan && isflexed())
+  {
+    openclaw();
+
+  }
+
+  
+}
+
+void closeclaw()
+{
       leftservo.write(92);
       rightservo.write(92);
-      delay(5000);
+      delay(1000);
       clawservo.write(12);
-    }  
-   /*if (garbagecount>9)
-    {
-    
-      delay(2000);
-      
+      delay(1000);
       leftservo.write(0);
       rightservo.write(180);
-      delay(2000);
-    }*/
-  
-  
-   
-  
- 
- 
-
+      hascan = true;
 
 }
+
+void openclaw()
+{
+      leftservo.write(92);
+      rightservo.write(92);
+      delay(1000);
+      clawservo.write(90);
+      delay(1000);
+      
+
+}
+
+
 
