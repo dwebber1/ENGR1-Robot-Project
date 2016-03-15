4b47db07a4f2c64b353ee40e08b42f27ab0cb240
Daniel Webber <dwebber1@users.noreply.github.com>
2016-03-14 19:58:14 -0700

    slight tweaks
    Working Version !

--- Code/servotest/servotest.ino
+++ Code/servotest/servotest.ino
@@ -1,73 +1,75 @@
-#include <Servo.h> 
+#include <Servo.h>
+
 int green = 3;
 int red = 2;
 int left = 6;
-int claw =9;
+int claw = 9;
 int right = 11;
 int flex = A0;
 int button = 12;
-int flexstore=0;
+int flexstore = 0;
 int garbagecount = 0;
+
 boolean hascan = false;
+
 Servo leftservo;
 Servo rightservo;
 Servo clawservo;
 
 
-void setup() 
-{ 
+void setup()
+{
   leftservo.attach(left);
   rightservo.attach(right);
   clawservo.attach(claw);
-  pinMode(button,INPUT_PULLUP);
-  pinMode(flex,INPUT);
-  pinMode(red,OUTPUT);
-  pinMode(green,OUTPUT);
-  Serial.begin(9600);
+  pinMode(button, INPUT_PULLUP);
+  pinMode(flex, INPUT);
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  //Serial.begin(9600);
   rightservo.write(93);
   leftservo.write(90);
-
   clawservo.write(90);
   delay(1500);
 
 
 
-//digitalWrite(red,1);
-digitalWrite(green,1);
-delay(1000);
-digitalWrite(red,0);
+  //digitalWrite(red,1);
+  digitalWrite(green, 1);
+  delay(1000);
+  digitalWrite(red, 0);
 
-delay(1000);
+  delay(1000);
 
-//digitalWrite(green,1);
-while (digitalRead(button))
-{}
+  //digitalWrite(green,1);
+  while (digitalRead(button))
+  {}
 
-digitalWrite(green,0);
-digitalWrite(red,1);
+  digitalWrite(green, 0);
+  digitalWrite(red, 1);
 
-rightservo.write(85);
-leftservo.write(98);
+  rightservo.write(85);
+  leftservo.write(99);
 
 
-  
-  
-    // set servo to mid-point
-} 
-  
+
+
+  // set servo to mid-point
+}
+
 boolean isflexed ()
 {
-    
-    return (analogRead(flex) <= 215);
-    
-  
- }
-void loop() 
+
+  return (analogRead(flex) <=190 );
+
+
+}
+void loop()
 {
- //if (digitalRead(button,HIGH){
-   // shouldmove = 1;    }
- Serial.println(analogRead(flex));
-  
+  //if (digitalRead(button,HIGH){
+  // shouldmove = 1;    }
+  //Serial.println(analogRead(flex));
+
   if (!hascan && isflexed())
   {
     while (garbagecount < 130)
@@ -78,45 +80,46 @@ void loop()
       else
         garbagecount = 0;
     }
-      closeclaw();
+    closeclaw();
   }
-  if(hascan && isflexed())
+  if (hascan && isflexed())
   {
     openclaw();
 
   }
 
-  
+
 }
 
 void closeclaw()
 {
-      leftservo.write(90);
-      rightservo.write(93);
-      
-      delay(1000);
-      
-      clawservo.write(20);
-      
-      delay(1000);
-      
-      rightservo.write(85);
-      leftservo.write(98);
-      
-      hascan = true;
-      delay(2000);
+  leftservo.write(90);
+  rightservo.write(93);
+
+  delay(1000);
+
+  clawservo.write(20);
+
+  delay(1000);
+  leftservo.write(99);
+  delay(100);
+  rightservo.write(85);
+  
+
+  hascan = true;
+  delay(2000);
 
 }
 
 void openclaw()
 {
-    
-      leftservo.write(90);
-      rightservo.write(93);
-      delay(1000);
-      clawservo.write(90);
-      delay(1000);
-      
+
+  leftservo.write(90);
+  rightservo.write(93);
+  delay(1000);
+  clawservo.write(90);
+  delay(1000);
+
 
 }
 
